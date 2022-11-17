82078cb67285da9ea31a3ed298ed5e665fd457d9
Amirhossein Rafiee <amirhossein.rafiee.80@gmail.com>
2022-11-17 05:27:02 +0330

    added deleteview

--- todo/urls.py
+++ todo/urls.py
@@ -6,4 +6,5 @@ app_name = 'todo'
 urlpatterns = [
     path('', views.TaskListView.as_view(), name='task-list'),
     path('create/', views.TaskCreateView.as_view(), name='task-create'),
+    path('delete/<int:pk>/', views.TaskDeleteView.as_view(), name='task-delete'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 # from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, DeleteView
 from .models import Task
 
 class TaskListView(ListView):
@@ -20,3 +20,7 @@ class TaskCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class TaskDeleteView(DeleteView):
+    model = Task
+    success_url = '/'
