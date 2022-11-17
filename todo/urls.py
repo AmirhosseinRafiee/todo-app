92bd2d86e07300cbd46eea906cb11bd106d592fa
Amirhossein Rafiee <amirhossein.rafiee.80@gmail.com>
2022-11-17 04:56:51 +0330

    added  createview

--- todo/urls.py
+++ todo/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'todo'
+
 urlpatterns = [
-    path('', views.TaskListView.as_view(), name='tasks-views'),
+    path('', views.TaskListView.as_view(), name='task-list'),
+    path('create/', views.TaskCreateView.as_view(), name='task-create'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,22 @@
 # from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
 from .models import Task
 
 class TaskListView(ListView):
-    template_name = 'task_list.html'
+    template_name = 'todo/task_list.html'
 
     context_object_name = 'tasks'
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
+
+
+class TaskCreateView(CreateView):
+    model = Task
+    fields = ('title',)
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
