9b7ad9c218e39dafbb4fd0065870c99b1743c8ad
Amirhossein Rafiee <amirhossein.rafiee.80@gmail.com>
2022-11-18 05:40:42 +0330

    added loginview

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
-# Create your views here.
+class LoginView(LoginView):
+    template_name = 'accounts/login.html'
+    fields = ('username', 'password')
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy("todo:task-list")
