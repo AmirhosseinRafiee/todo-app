2b95ee903a4694e29f5c0e9d574fb0e72f499196
Amirhossein Rafiee <amirhossein.rafiee.80@gmail.com>
2022-11-18 06:46:34 +0330

    added Registerview and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
@@ -5,4 +6,6 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
+    path('register/', views.RegisterView.as_view(), name='register'),
+    path('logout/', LogoutView.as_view(next_page='accounts:login'), name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
-# from django.shortcuts import render
+from django.shortcuts import redirect
 from django.contrib.auth.views import LoginView
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 
 class LoginView(LoginView):
@@ -9,3 +11,13 @@ class LoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy("todo:task-list")
+
+class RegisterView(CreateView):
+    template_name = 'accounts/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy("accounts:login")
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/')
+        return super().get(request, *args, **kwargs)
