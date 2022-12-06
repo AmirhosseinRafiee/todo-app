6a510e8547eb80834caf916a7d504ffd44d7020e
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2022-12-07 02:41:12 +0330

    adding  filter backend, ordering and searchfilter

--- todo/api/v1/views.py
+++ todo/api/v1/views.py
@@ -1,11 +1,17 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from ...models import Task
 from .serializers import TaskSerializer
 
 class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
     serializer_class = TaskSerializer
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = ['is_completed']
+    search_fields = ['title']
+    ordering_fields = ['created_date', 'updated_date']
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
