e42941410babf52bdb3fa854fc6e7372202cd9b1
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2022-12-06 20:38:21 +0330

    modelviewset for task

---
+++ todo/api/v1/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from ...models import Task
+
+class TaskSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Task
+        fields = ('id', 'user', 'title', 'is_completed', 'created_date', 'updated_date')
