a490427cfdadf8d07b5fbf3670311daba8479d08
Amirhossein Rafiee <94180638+AmirhosseinRafiee@users.noreply.github.com>
2022-12-07 02:56:48 +0330

    adding custom pagination class

--- todo/api/v1/views.py
+++ todo/api/v1/views.py
@@ -4,6 +4,7 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from django_filters.rest_framework import DjangoFilterBackend
 from ...models import Task
 from .serializers import TaskSerializer
+from .paginations import DefaultPagination
 
 class TaskViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticatedOrReadOnly]
@@ -12,6 +13,7 @@ class TaskViewSet(viewsets.ModelViewSet):
     filterset_fields = ['is_completed']
     search_fields = ['title']
     ordering_fields = ['created_date', 'updated_date']
+    pagination_class = DefaultPagination
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
